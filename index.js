273169fa6cfccb3ed02e653d37b6307ab6142137
Argudo <josexerezano7@gmail.com>
2020-08-03 02:21:21 +0200

    tk diego

--- index.js
+++ index.js
@@ -98,11 +98,16 @@ bot.on('message', msg=>{
         isReady = true;
         msg.delete();
     }
+    else if(msg.content === 'tk' && isReady){
+        playAudio('18',msg);
+        isReady = true;
+        msg.delete();
+    }
 })
 
 bot.on('message', msg=>{
     if(msg.content === '.list'){
-        msg.reply('\n1 - Alto Puto\n2 - Father on father\n3 - DjAbascal\n4 - Fvirgolini\n5 - Subnormal\n6 - Doraimon\n7 - Yeet\n8 - Insultos\n9 - Anda con Dioh\n10 - Mongolo\n11 - Armagedon\n12 - Me sua la polla\n13 - Cuaderno de cuadrito\n14 - Enzarpaisimo\n15 - Bebio\n16 - Respeto');
+        msg.reply('\n1 - Alto Puto\n2 - Father on father\n3 - DjAbascal\n4 - Fvirgolini\n5 - Subnormal\n6 - Doraimon\n7 - Yeet\n8 - Insultos\n9 - Anda con Dioh\n10 - Mongolo\n11 - Armagedon\n12 - Me sua la polla\n13 - Cuaderno de cuadrito\n14 - Enzarpaisimo\n15 - Bebio\n16 - Respeto\n17 - Depredador');
         let filter = m => !m.author.bot;
         let collector = new Discord.MessageCollector(msg.channel, filter, {max:1});
         collector.on('collect', (m, col) => {
@@ -278,6 +283,13 @@ function playAudio(num, msg){
             msg.member.voice.channel.join()
             .then(connection => {
                 const dispatcher = connection.play(require("path").join(__dirname, './depredador.mp3'));
+                dispatcher.on('finish', finish => voiceChannel.leave());
+        })
+        case '18':
+            var voiceChannel = msg.member.voice.channel;
+            msg.member.voice.channel.join()
+            .then(connection => {
+                const dispatcher = connection.play(require("path").join(__dirname, './tequiero.mp3'));
                 dispatcher.on('finish', finish => voiceChannel.leave());
         })
     }
